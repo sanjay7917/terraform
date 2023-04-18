2111892049ba05d75a069a174aaa9e771c500827
Sanju <killu@Sanjus-MacBook-Air.local>
2023-04-19 00:08:10 +0530

    new change to file has been added

---
+++ aws-ec2-keypair-sg-vpc/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "default"
+}
