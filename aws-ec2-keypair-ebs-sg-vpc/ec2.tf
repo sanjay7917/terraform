adce15a3156c2251e769f2c808e4f970427e68e9
Sanju <killu@Sanjus-MacBook-Air.local>
2023-04-19 21:03:01 +0530

    key change

--- aws-ec2-keypair-ebs-sg-vpc/ec2.tf
+++ aws-ec2-keypair-ebs-sg-vpc/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "server" {
   ami                         = "ami-06c4532923d4ba1ec"
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.key.key_name
+  key_name                    = aws_key_pair.this.key_name
   vpc_security_group_ids      = ["${aws_security_group.sg.id}"]
   subnet_id                   = aws_subnet.pub_sub.id
   associate_public_ip_address = true
--- aws-ec2-keypair-ebs-sg-vpc/keypair.tf
+++ aws-ec2-keypair-ebs-sg-vpc/keypair.tf
@@ -1,5 +1,12 @@
-resource "aws_key_pair" "key" {
+# resource "aws_key_pair" "key" {
+#   key_name   = "terra"
+#   public_key = file("${path.module}/id_rsa.pub")
+# }
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "this" {
   key_name   = "terra"
-  public_key = file("${path.module}/id_rsa.pub")
-
+  public_key = tls_private_key.this.public_key_openssh
 }
