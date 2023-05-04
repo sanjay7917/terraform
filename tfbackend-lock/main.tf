a0f20222cb823f3b10d709716b5c838577dc4fca
Sanju <killu@Sanjus-MacBook-Air.local>
2023-05-04 14:02:13 +0530

    s3 backend removed from god-tier

---
+++ tfbackend-lock/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region  = "us-east-2"
+  profile = "default"
+}
+# terraform {
+#   backend "s3" {
+#     bucket         = "lockingtest123456"
+#     region         = "us-east-2"
+#     key            = "terraform.tfstate"
+#     dynamodb_table = "my-table"
+#   }
+# }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
